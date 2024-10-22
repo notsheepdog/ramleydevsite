909e2cf7619c7ac8b33acbe1f975375928fc558f
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2024-10-22 12:37:49 -0400

    Back to OG config

--- assets/config.js
+++ assets/config.js
@@ -1,22 +1,69 @@
 let config = {
-  "Title": "Ramley - Game Dev",
-  "Name": "Ramley",
-  "Description": "Game Developer - Formally known as Sheepdog",
+  "Title": "Your Name - Links",
+  "Name": "Your Name",
+  "Description": "Your Description",
   "links": [
     {
-      "Title": "Portfolio",
-      "URL": "https://sheepshouse.notion.site/Games-by-J-F-Ramley-c03c8f8f4f0c44b9a756edba9a851bfc",
-      "icon_classes": "fas fa-link"
+      "Title": "Website",
+      "URL": "http://www.harindu.dev",
+      "icon_classes": "fas fa-link",
     },
     {
-      "Title": "Itch.io",
-      "URL": "https://ramleydev.itch.io/",
-      "icon_classes": "fas fa-link"
+      "Title": "Github",
+      "URL": "https://github.com/harindulk",
+      "icon_classes": "fab fa-github",
+    },
+    {
+      "Title": "Play Store",
+      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
+      "icon_classes": "fab fa-google-play"
+    },
+    {
+      "Title": "PC Games",
+      "URL": "https://harindulk.itch.io/",
+      "icon_classes": "fab fa-itch-io"
+    },
+    {
+      "Title": "Blog",
+      "URL": "blog-test",
+      "icon_classes": "fas fa-blog"
     },
     {
       "Title": "Twitter",
-      "URL": "https://x.com/notsheepdog",
-      "icon_classes": "fas fa-link"
-    }
+      "URL": "https://twitter.com/Harindu_Fonseka",
+      "icon_classes": "fab fa-twitter"
+    },
+    {
+      "Title": "LinkedIn",
+      "URL": "https://www.linkedin.com/in/harindulk/",
+      "icon_classes": "fab fa-linkedin"
+    },
+    {
+      "Title": "Youtube",
+      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
+      "icon_classes": "fab fa-youtube"
+    },
   ]
 }
+// let config = {
+//   "Title": "Ramley - Game Dev",
+//   "Name": "Ramley",
+//   "Description": "Game Developer - Formally known as Sheepdog",
+//   "links": [
+//     {
+//       "Title": "Portfolio",
+//       "URL": "https://sheepshouse.notion.site/Games-by-J-F-Ramley-c03c8f8f4f0c44b9a756edba9a851bfc",
+//       "icon_classes": "fas fa-link"
+//     },
+//     {
+//       "Title": "Itch.io",
+//       "URL": "https://ramleydev.itch.io/",
+//       "icon_classes": "fas fa-link"
+//     },
+//     {
+//       "Title": "Twitter",
+//       "URL": "https://x.com/notsheepdog",
+//       "icon_classes": "fas fa-link"
+//     }
+//   ]
+// }
