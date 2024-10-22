be3b2ee17ad23976fa0c8b73ac0bee2435320e46
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2024-10-22 12:11:30 -0400

    Updated config.js with site contents

--- assets/config.js
+++ assets/config.js
@@ -1,47 +1,22 @@
 let config = {
-  "Title": "Your Name - Links",
-  "Name": "Your Name",
-  "Description": "Your Description",
+  "Title": "Ramley - Game Dev",
+  "Name": "Ramley",
+  "Description": "Game Developer - Formally known as Sheepdog",
   "links": [
     {
-      "Title": "Website",
-      "URL": "http://www.harindu.dev",
-      "icon_classes": "fas fa-link",
+      "Title": "Portfolio",
+      "URL": "https://sheepshouse.notion.site/Games-by-J-F-Ramley-c03c8f8f4f0c44b9a756edba9a851bfc",
+      "icon_classes": "fas fa-link"
     },
     {
-      "Title": "Github",
-      "URL": "https://github.com/harindulk",
-      "icon_classes": "fab fa-github",
-    },
-    {
-      "Title": "Play Store",
-      "URL": "https://play.google.com/store/apps/dev?id=6729187126243636577",
-      "icon_classes": "fab fa-google-play"
-    },
-    {
-      "Title": "PC Games",
-      "URL": "https://harindulk.itch.io/",
-      "icon_classes": "fab fa-itch-io"
-    },
-    {
-      "Title": "Blog",
-      "URL": "blog-test",
-      "icon_classes": "fas fa-blog"
+      "Title": "Itch.io",
+      "URL": "https://ramleydev.itch.io/",
+      "icon_classes": "fas fa-link"
     },
     {
       "Title": "Twitter",
-      "URL": "https://twitter.com/Harindu_Fonseka",
-      "icon_classes": "fab fa-twitter"
-    },
-    {
-      "Title": "LinkedIn",
-      "URL": "https://www.linkedin.com/in/harindulk/",
-      "icon_classes": "fab fa-linkedin"
-    },
-    {
-      "Title": "Youtube",
-      "URL": "https://www.youtube.com/channel/UCRyQGxzCgFb5wmsp1XAlWpQ",
-      "icon_classes": "fab fa-youtube"
-    },
+      "URL": "https://x.com/notsheepdog",
+      "icon_classes": "fas fa-link"
+    }\
   ]
 }
