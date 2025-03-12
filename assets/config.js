e696698c27a027835a61c1a5546db18a56d133b0
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2025-03-12 15:18:09 -0400

    Added HR Steam page

--- assets/config.js
+++ assets/config.js
@@ -4,6 +4,11 @@ let config = {
   "Description": "Game Developer - Formerly known as Sheepdog",
   "links": [
     {
+      "Title": "Wishlist Heretic Red on Steam",
+      "URL": "https://store.steampowered.com/app/3549760/Heretic_Red/",
+      "icon_classes": "fas fa-link"
+    },
+    {
       "Title": "Portfolio",
       "URL": "https://ramley.notion.site/Games-by-J-F-Ramley-c03c8f8f4f0c44b9a756edba9a851bfc",
       "icon_classes": "fas fa-link"
